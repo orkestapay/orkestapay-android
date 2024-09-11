2440597039dc5eb4421707225e2c7e6b3a0d233e
Hector Rodriguez <hector.rodriguez@zenki.fi>
2024-09-11 12:40:11 -0600

    click to pay add user data

--- Orkestapay/src/main/java/com/orkestapay/orkestapay/client/model/clicktopay/ClickToPay.kt
+++ Orkestapay/src/main/java/com/orkestapay/orkestapay/client/model/clicktopay/ClickToPay.kt
@@ -2,11 +2,7 @@ package com.orkestapay.orkestapay.client.model.clicktopay
 
 import java.io.Serializable
 
-data class ClickToPay(val srcDpaId: String,
-                      val dpaLocale: String,
-                      val dpaName: String,
-                      val cardBrands: List<String>,
-                      val email: String,
+data class ClickToPay(val email: String? = null,
                       val firstName: String? = null,
                       val lastName: String? = null,
                       val phoneCountryCode: String? = null,
