e8f7f4952d571d666496a614de18ac5bcbddfab5
Hector Rodriguez <hector.rodriguez@zenki.fi>
2024-09-12 13:29:45 -0600

    changes click to pay

--- Orkestapay/src/main/java/com/orkestapay/orkestapay/client/model/PaymentMethodResponse.kt
+++ Orkestapay/src/main/java/com/orkestapay/orkestapay/client/model/PaymentMethodResponse.kt
@@ -6,6 +6,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class PaymentMethodResponse(@SerialName("payment_method_id") val paymentMethodId: String,
-                                 val alias: String,
+                                 val alias: String? = null,
                                  val type: PaymentMethodType,
                                  val card: CardResponse)
