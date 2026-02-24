46338f39010e91691c6092acfda83b09df86bc92
Hector Rodriguez <hector.rodriguez@zenki.fi>
2026-02-23 19:26:48 -0600

    version

--- Orkestapay/build.gradle.kts
+++ Orkestapay/build.gradle.kts
@@ -2,7 +2,7 @@ import com.vanniktech.maven.publish.SonatypeHost
 
 val groupId by extra { "com.orkestapay" }
 val artifactId by extra { "orkestapay" }
-val libraryVersion by extra { "0.1.4" }
+val libraryVersion by extra { "0.1.5" }
 
 plugins {
     alias(libs.plugins.androidLibrary)
